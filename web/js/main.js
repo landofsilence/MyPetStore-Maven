5bed95f34df88359d76de57300b039e7d939bf70
jxnflzc <245186727@qq.com>
2019-01-04 14:55:31 +0800

    Second

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    var tops = $(".swiper-card-wrapper").offset().top + $(".swiper-card-wrapper").height() / 3;
-    console.log("$(\".swiper-card-wrapper\").offset().top = " + $(".swiper-card-wrapper").offset().top)
-    console.log("$(\".swiper-card-wrapper\").offset().height / 2 = " + $(".swiper-card-wrapper").height() / 3)
+    var tops = $(".swiper-card-wrapper").offset().top + $(".swiper-card-wrapper").height() / 2 - $(".btn-card-prev").height() / 2;
     $(".btn-card-prev").css("top", tops);
     $(".btn-card-next").css("top", tops);
 })
