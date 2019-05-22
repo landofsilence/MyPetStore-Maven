5cd264f1321bff8bd5231bafbd108c3a56344450
xiayu <853172836@qq.com>
2019-05-22 19:50:20 +0800

    5.22/19.50

---
+++ src/per/jxnflzc/web/action/CheckoutAction.java
@@ -0,0 +1,16 @@
+package per.jxnflzc.web.action;
+
+import com.opensymphony.xwork2.ModelDriven;
+import per.jxnflzc.domain.Cart;
+import com.opensymphony.xwork2.Action;
+
+
+public class CheckoutAction implements Action {
+
+
+    @Override
+    public String execute() throws Exception {
+        return "checkout";
+    }
+
+}
