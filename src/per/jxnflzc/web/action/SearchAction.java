6417459dd1e0013ec0414ec6f0ca01c23e15d401
ASUS <1443248105@qq.com>
2019-05-22 22:19:11 +0800

    5/22/22:18 Search功能

--- src/per/jxnflzc/web/action/SearchAction.java
+++ src/per/jxnflzc/web/action/SearchAction.java
@@ -2,31 +2,37 @@ package per.jxnflzc.web.action;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionContext;
+import per.jxnflzc.domain.Category;
 import per.jxnflzc.domain.Product;
 import per.jxnflzc.service.CategoryService;
 
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author 河木
- * @version v1.0.0
- */
 public class SearchAction implements Action {
-	private String keyword;
+    private String keyword;
+    private Category category = new Category();
 
-	public String getKeyword() {
-		return keyword;
-	}
+    public String getKeyword() {
+        return keyword;
+    }
 
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
 
-	@Override
-	public String execute() throws Exception {
-		return null;
-	}
+    @Override
+
+    public String execute() throws Exception {
+        ActionContext context = ActionContext.getContext();
+        Map session = context.getSession();
+        CategoryService categoryService = new CategoryService();
+
+        List<Product> productList = categoryService.searchProductList(getKeyword());
+        session.put("productList", productList);
+
+        return "success";
+    }
 
 	public void completion() throws Exception  {
 		ActionContext context = ActionContext.getContext();
